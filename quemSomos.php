60a43b91feecb268cb4110d7a68753ee7980ce9a
Eduardo Iglesias Thompson <edu_thompson@hotmail.com>
2020-01-29 20:53:52 -0300

    quem somos atualizado

--- quemSomos.php
+++ quemSomos.php
@@ -28,57 +28,29 @@ var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s,
 
 <body>
 
-<!-- // Card deck Redes sociais -->
-
-<div class="card-deck">
-  <div class="card">
-    <img src=assets\img\facebook.png class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Facebook</h5>
-      <p class="card-text">Curta nossa pagina para ficar atualizado de tudo que acontece, inclusive eventos especiais e descontos exclusivos!</p>
-
-    </div>
-  </div>
-  <div class="card">
-    <img src=assets\img\instagram.jpg class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Instagram</h5>
-      <p class="card-text">Fique por dentro de novos sabores e promoções, além de conferir as fotos de cada uma de nossas pizzas!</p>
-
-    </div>
-  </div>
-  <div class="card">
-    <img src=assets\img\blog.png class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Blog/Notícias</h5>
-      <p class="card-text">Fique por dentro das nossas novidades atraves do nosso blog! Lá você também encontra notícias e curiosidades sobre a nossa pizzaria! .</p>
-
-    </div>
-  </div>
-</div>
-
-
-<div class="card mb-3" style="max-width: 2000px;">
+<section>
+<div container="md-5">
+<div class="card mb-4" style="max-width: 2000px;">
   <div class="row no-gutters">
-    <div class="col-md-4" id="legopizza">
-      <img src="assets\img\starwars.jpg" class="card-img" alt="...">
-    </div>
-    <div class="col-md-8">
+  <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title">Quem somos</h5>
+        <h5 class="card-title" >Quem somos</h5>
+        <div style="width: 400px">
         <p class="card-text">
 
-        insert.seuPedido {<br> 
-        A Codpizza é a sua pizzaria, <br>
-        Deliciosos sabores programados para o seu paladar, rápida na entrega como 5g e inovadora como os tempos de hoje. <br> 
-        Experiemente essa inovação! Viva o futuro!<br>
-         }<br>
-         return $sempre!</p>
+        Desde 1971 a Pizzaria do Angelo, está localizada em um dos principais bairros de São Paulo, com muito orgulho em fazer parte do coração da Mooca, levamos como principal conceito, a satisfação dos nossos clientes e também aquela interação diferenciada e duradoura.<br><br>
 
+E para você que almeja estar em um ambiente tradicional e familiar e admira uma bela pizza,. Venha conhecer à Pizzaria do Angelo, que tem como opção de atendimento, o nosso salão ou o famoso balcão de Pizzas da Mooca. Aguardamos por você e sua família.<br>
+          </div>
       </div>
+    </div>
+    <div class="col-md-4" id="legopizza">
+      <img src="assets\img\historia.jpg" class="card-img" alt="...">
     </div>
   </div>
 </div>
+</section>
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
